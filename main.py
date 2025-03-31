6b2735941f30a60531a3e2ee9b448ae505e1a71a
dcr29 <170249498+dcr29@users.noreply.github.com>
2025-03-31 22:55:27 +0200

    classe button faite, affichage ok, clic détécté ok

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import pygame
 from Ball import Ball
+from Button import Button
 
 pygame.init()
     
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Mastermind")
+
 screen.fill((255, 255, 255))
+button = Button(500, 500, 50, 50, "Jouer", (255, 0, 0), 20)
+ball1 = Ball(400,300,50,(255,0,255))
 
 running = True
-ball1 = Ball(400,300,50,(255,0,255))
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             running = False
-        elif event.type == pygame.MOUSEBUTTONUP: #si click
-            if event.button == 1: #si c'est un click droit
+        elif event.type == pygame.MOUSEBUTTONUP:    # si clic
+            if event.button == 1:   # si c'est un clic gauche
                 ball1.is_clicked(event.pos)
-                    
+                
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button.is_clicked(event.pos):
+                print("Clic détécté sur le bouton")
+
+    button.draw(screen)        
     ball1.draw(screen)
+    
     pygame.display.flip()
 
 pygame.quit()
