bf0266598b57be30808613edfcdc23b3deaf9bbb
Natkbz <n02knat@outlook.fr>
2025-03-31 16:37:11 +0200

    main qui affiche une ball

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
+from Ball import Ball
 
 pygame.init()
     
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Mastermind")
-    
+screen.fill((255, 255, 255))
+
 running = True
+ball1 = Ball(400,300,50,(255,0,255))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             running = False
+    ball1.draw(screen)
+    pygame.display.flip()
 
 pygame.quit()
