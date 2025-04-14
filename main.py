6b563773a706a167477e29d7fc9c0826e77bd625
Natkbz <n02knat@outlook.fr>
2025-04-14 17:40:51 +0200

    Colorpalette affichable, is-clicked non definit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from Ball import Ball
+from ColorPalette import ColorPalette
 from Button import Button
 from Hole import Hole
 from DoneLine import DoneLine
@@ -10,13 +10,13 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Mastermind")
 
 screen.fill((255, 255, 255))
-button = Button(500, 500, 50, 50, "Jouer", (255, 0, 0), 20)
-ball1 = Ball(400,300,50,(255,0,255))
-
 running = True
 
-ball1 = Ball(400,550,50,(255,0,255))
+#déclaration des objets :
 hole = Hole(400,300,50,(100,100,100))
+button = Button(500, 500, 50, 50, "Jouer", (255, 0, 0), 20)
+avaibleColor = [(0, 0, 255), (255, 192, 203), (255, 0, 0), (0, 255, 0), (255, 255, 0), (255, 165, 0), (238, 130, 238), (255, 255, 255)]
+colorPalette = ColorPalette(4,screen.get_width(),screen.get_height(),avaibleColor)
 color=()
 test_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)]
 done_line = DoneLine(test_colors, 800, 600, correctCount=2, wrongPlaceCount=1)
@@ -29,14 +29,14 @@ while running:
 
         elif event.type == pygame.MOUSEBUTTONUP: # si click
             if event.button == 1: # si c'est un click gauche
-                clicked = ball1.is_clicked(event.pos)
+                clicked = colorPalette.is_clicked(event.pos)
                 if clicked :
                     color = clicked
                 hole.is_clicked(event.pos,color)
                 if button.is_clicked(event.pos):
                     print("Clic détécté sur le bouton")
                     
-    ball1.draw(screen)
+    colorPalette.draw(screen)
     button.draw(screen)
     hole.draw(screen)
     done_line.draw(screen)
